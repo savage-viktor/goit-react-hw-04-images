aa2c1dd64f9122ad7c8fa00579feaf4d454ad265
Viktor <93682529+savage-viktor@users.noreply.github.com>
2023-07-25 21:38:59 +0300

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,39 +1,66 @@
 // import PropTypes from "prop-types";
 import styles from "./Modal.module.css";
 import { createPortal } from "react-dom";
-import { Component } from "react";
+import { useEffect } from "react";
 
 const modalRoot = document.querySelector("#modal");
 
-class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener("keydown", this.listener);
-    window.addEventListener("click", this.listener);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener("keydown", this.listener);
-    window.removeEventListener("click", this.listener);
-  }
-
-  listener = (event) => {
+const Modal = ({ onClose, image, description }) => {
+  const listener = (event) => {
     if (event.code === "Escape" || event.target.id === "modal-overlay") {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  render() {
-    const { image, description } = this.props;
-    return createPortal(
-      <div id="modal-overlay" className={styles.overlay}>
-        <div className={styles.modal}>
-          <img src={image} alt={description} />
-        </div>
-      </div>,
-      modalRoot
-    );
-  }
-}
+  useEffect(() => {
+    window.addEventListener("keydown", listener);
+    window.addEventListener("click", listener);
+    return () => {
+      window.removeEventListener("keydown", listener);
+      window.removeEventListener("click", listener);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return createPortal(
+    <div id="modal-overlay" className={styles.overlay}>
+      <div className={styles.modal}>
+        <img src={image} alt={description} />
+      </div>
+    </div>,
+    modalRoot
+  );
+};
+
+// class Modal extends Component {
+//   componentDidMount() {
+//     window.addEventListener("keydown", this.listener);
+//     window.addEventListener("click", this.listener);
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener("keydown", this.listener);
+//     window.removeEventListener("click", this.listener);
+//   }
+
+//   listener = (event) => {
+//     if (event.code === "Escape" || event.target.id === "modal-overlay") {
+//       this.props.onClose();
+//     }
+//   };
+
+//   render() {
+//     const { image, description } = this.props;
+//     return createPortal(
+//       <div id="modal-overlay" className={styles.overlay}>
+//         <div className={styles.modal}>
+//           <img src={image} alt={description} />
+//         </div>
+//       </div>,
+//       modalRoot
+//     );
+//   }
+// }
 
 // Modal.propTypes = {};
 
